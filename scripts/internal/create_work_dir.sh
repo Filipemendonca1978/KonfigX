7f096e76e7e412b675d57b1af118bbce2377951a
K0nf1gur@r3 Ph0n3s <someone12334473@gmail.com>
2026-04-04 19:35:02 +0300

    Update create_work_dir.sh

--- scripts/internal/create_work_dir.sh
+++ scripts/internal/create_work_dir.sh
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 # [
 source "$SRC_DIR/scripts/utils/build_utils.sh" || exit 1
